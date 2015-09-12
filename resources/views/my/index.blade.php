86ec599c1a99c1ddbea5fba33a31c49f3d7a8adb
zhangxiatian2s <506834853@qq.com>
2015-09-12 22:40:37 +0800

    个人中心左边

--- resources/views/my/index.blade.php
+++ resources/views/my/index.blade.php
@@ -1,61 +1,42 @@
 @extends('base')
-
 @section('content')
+<link href="{{ asset('/css/personcenter.css') }}" rel="stylesheet">
 <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
-
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> Remember Me
-									</label>
-								</div>
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">Login</button>
-
-								<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
+	<div class="row person_center_wrap">
+		<div class="person_nav_left">
+			<ul>
+				<li class="current">
+					<a href="#">
+						注册信息管理
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						已发布信息
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						站内信
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						修改密码
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						注销用户
+					</a>
+				</li>
+			</ul>
+			<div class="person_nav_left_bc"></div>
 		</div>
+		<div class="person_center_right">
+			
+		</div>
+		
 	</div>
 </div>
 @endsection
