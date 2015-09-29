826158886fee0a702426d0aac76f0980df8ac281
赵晓龙 <99866770@qq.com>
2015-09-29 14:06:41 +0800

    修改staff model的默认表名

--- app/Staff.php
+++ app/Staff.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Eloquent\Model;
 
 class Staff extends Model {
-
+  protected $table = 'staffs';
 	protected $fillable = ['user_id', 'work_category_id', 'work_category_name', 'start_at', 'end_at', 'province', 'city', 'street', 'area_name', 'address', 'title'];
 
 }
