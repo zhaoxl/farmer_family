493116ab815e129dcc3cb49f996697349488976d
backkom <99866770@qq.com>
2015-12-26 20:53:09 +0800

    修改首页样式

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,12 @@
 $(function(){
+	//注册按钮
+	$(".reg_btn").mouseover(function(){
+		$(this).find("div").show();
+	}).mouseout(function(){
+		$(this).find("div").hide();
+	});
+	
+	//登陆框
 	$("#index .login_box .mobile").focus(function(){
 		if($(this).val() == "请输入手机号")
 		{
@@ -22,4 +30,51 @@ $(function(){
 			$(this).val("请输入密码").attr("type", "text");
 		}
 	});
+	
 });
+
+  var validate = $("#login_form").validate({
+    //errorClass: "label.error", //默认为错误的样式类为：error   
+    focusInvalid: true, //当为false时，验证无效时，没有焦点响应  
+    onkeyup: false,   
+    submitHandler: function(form){   //表单提交句柄,为一回调函数，带一个参数：form   
+    	form.submit();   //提交表单   
+    },   
+		errorPlacement: function(error, element) {
+			$("#login_form input.error").each(function() {
+				if ($(element).is(":checkbox"))
+				{
+					$(element).parent().css("color", "red");
+				}
+				else
+				{
+					$(element).attr($(element).attr('html'), $(error).html());
+				}
+			});
+		},   
+    rules:{
+			mobile:{
+			  required:true,
+			  phone:true
+			},
+			password:{
+				required:true,
+				rangelength:[3,10],
+				pwd: true
+			}
+    },
+    messages:{
+      mobile:{
+      	required:"请输入手机号",
+      	phone:'手机号码格式错误'
+      },
+		 password:{
+		   required: "请输入密码",
+		 }
+   }
+});    
+jQuery.validator.addMethod("phone", function(value, element) {
+  var length = value.length;
+  var mobile = /^(((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})$/
+  return this.optional(element) || (length == 11 && mobile.test(value));
+}, "手机号码格式错误");  
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,14 +9,19 @@
 	<link href="{{ asset('/css/index.css') }}" rel="stylesheet">
 </head>
 <body>
-	<script src="/js/jquery.min.js"></script>
-	<script src="/js/custom-form-elements.js"></script>
-	<script src="/js/index.js"></script>
 	<?php echo View::make('partials.index_header') ?>
 	<div id="index">
 		<div class="context">
-			<a href="/auth/create" class="reg_btn1"></a>
-			<a href="/auth/create" class="reg_btn2"></a>
+			<a href="/auth/create" class="reg_btn reg_btn1">
+				<div class="reg_btn_hover">
+					注册后可以发布生活服务信息
+				</div>
+			</a>
+			<a href="/auth/create" class="reg_btn reg_btn2">
+				<div class="reg_btn_hover">
+					注册后可以发布生活服务信息
+				</div>
+			</a>
 			<div class="login_box">
 				<form id="login_form" class="form-horizontal" method="POST" action="{{ url('/auth/login') }}">
 					<input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -52,8 +57,7 @@
 									<span class="block1">{{$staff->user->name}}</span>
 									<span class="block1">{{$staff->user->gender}}</span>
 									<span class="block1">{{$staff->user->age()}}岁</span>
-									<span class="block1">{{substr($user->workCategoryNames(), 0, 12)}}</span>
-									<span class="block2">{{substr($staff->user->address, 0, 27)}}</span>
+									<span class="block3">{{substr($user->workCategoryNames(), 0, 39)}}</span>
 									<span class="block1">【{{date('Y-m-d', strtotime($staff->created_at))}}】</span>
 								</a>
 							</li>
@@ -79,17 +83,16 @@
 						@if(!is_null($user))
 							<li>
 								<a href="/works/{{$work->id}}" class="{{$work->flag ? 'hot':''}}">
-									<span class="block2">
+									<span class="block3">
 									<?php #企业用户?>
 									@if($user->category == 1)
-									{{substr($work->companyName(), 0, 27)}}
+									{{substr($work->companyName(), 0, 30)}}
 									@else
 									<?php #个人用户?>
 									{{$work->name}}
 									@endif
 									</span>
-									<span class="block2">{{substr($work->work_category_name, 0, 12)}}</span>
-									<span class="block2">{{substr($work->address, 0, 27)}}&nbsp;</span>
+									<span class="block3">{{substr($work->work_category_name, 0, 30)}}</span>
 								</a>
 								<span class="block1">【{{date('Y-m-d', strtotime($work->created_at))}}】</span>
 							</li>
@@ -116,55 +119,9 @@
 			</div>
 		</div>
 	</div>
+	<script src="/js/jquery.min.js"></script>
+	<script src="/js/custom-form-elements.js"></script>
 	<script type="text/javascript" src="{{ asset('/js/jquery.validate.min.js') }}" ></script>
-	<script>
-	  var validate = $("#login_form").validate({
-	    //errorClass: "label.error", //默认为错误的样式类为：error   
-	    focusInvalid: true, //当为false时，验证无效时，没有焦点响应  
-	    onkeyup: false,   
-	    submitHandler: function(form){   //表单提交句柄,为一回调函数，带一个参数：form   
-	    	form.submit();   //提交表单   
-	    },   
-			errorPlacement: function(error, element) {
-				$("#login_form input.error").each(function() {
-					if ($(element).is(":checkbox"))
-					{
-						$(element).parent().css("color", "red");
-					}
-					else
-					{
-						$(element).attr($(element).attr('html'), $(error).html());
-					}
-				});
-			},   
-	    rules:{
-				mobile:{
-				  required:true,
-				  phone:true
-				},
-				password:{
-					required:true,
-					rangelength:[3,10],
-					pwd: true
-				}
-	    },
-	    messages:{
-	      mobile:{
-	      	required:"请输入手机号",
-	      	phone:'手机号码格式错误'
-	      },
-			 password:{
-			   required: "请输入密码",
-			 }
-	   }
-	});    
-  jQuery.validator.addMethod("phone", function(value, element) {
-    var length = value.length;
-    var mobile = /^(((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})$/
-    return this.optional(element) || (length == 11 && mobile.test(value));
-	}, "手机号码格式错误");  
-	
-	
-	</script>
+	<script src="/js/index.js"></script>
 </body>
 </html>
